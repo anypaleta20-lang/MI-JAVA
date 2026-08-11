0d99f5bce42c25a952aa2d6242e469cd8d82f817
anypaleta20-lang <any.paleta20@gmail.com>
2026-08-11 15:46:45 +0000

    S02

---
+++ S01/POO/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Triangulo fig1 = new Triangulo(10, 6);
+
+        System.out.println("El área dek triangulo es: " + fig1.calcularArea());
+    }
+}
+
