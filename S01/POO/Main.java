b5be3f091f16f5447d465a8241d3471b3e587bf4
anypaleta20-lang <any.paleta20@gmail.com>
2026-08-11 15:54:18 +0000

    S02

--- S01/POO/Main.java
+++ S01/POO/Main.java
@@ -1,8 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Triangulo fig1 = new Triangulo(10, 6);
+        Triangulo fig2 = new Triangulo(15, 5);
+        Triangulo fig3 = new Triangulo(18, 9);
 
-        System.out.println("El área dek triangulo es: " + fig1.calcularArea());
+        System.out.println("El área del triangulo es: " + fig1.calcularArea());
+        System.out.println("El área del triangulo es: " + fig2.calcularArea());
+        System.out.println("El área del triangulo es: " + fig3.calcularArea());
     }
 }
-
