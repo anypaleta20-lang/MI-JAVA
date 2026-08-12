9013f3ed897ead6897806b89d7848ff401bf1b5b
anypaleta20-lang <any.paleta20@gmail.com>
2026-08-12 15:44:09 +0000

    S02

---
+++ S03/Persona.java
@@ -0,0 +1,14 @@
+public class Persona {
+
+    String nombre;
+    int edad;
+
+    public Persona (String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void presentarse() {
+        System.out.println("Hola, soy " + nombre + " y tengo " + edad + " años.");
+    }
+}
