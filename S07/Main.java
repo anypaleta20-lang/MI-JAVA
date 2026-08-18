2dff29cacc304fd824123edcd608cf640540207e
anypaleta20-lang <any.paleta20@gmail.com>
2026-08-18 15:38:45 +0000

    S04

---
+++ S07/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("======================= ACTIVIDAD 06 =======================");
+        Celular miCelular = new Celular("Oppo", "Reno7", "Android 11", 256);
+       
+        miCelular.encender();
+        //miCelular.mostrarInfo();
+        miCelular.mostrarDetalles();
+        miCelular.tomarFoto();
+    }
+}    
